1fc3f44484787792ebd13d1f7345d82abb819dbe
spencerwooo <spencerwoo98@foxmail.com>
2020-03-21 22:39:20 +0800

    Refactor blackbox attack with separate utils

---
+++ blackbox_attack_foolbox/blackbox_attack_gen_adv.py
@@ -0,0 +1,143 @@
+"""
+Perform HopSkipJumpAttack on CNNs
+
+* Generated adversaries are saved inside 'advs/hop_skip_jump_attack_adv.npy'
+"""
+
+import os
+import time
+
+import foolbox
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
+from utils import utils
+
+# pretrained model state_dict path
+MODEL_RESNET_PATH = '../resnet_foolbox/200224_0901_resnet_imagenette.pth'
+MODEL_VGG_PATH = '../vgg_foolbox/200224_0839_vgg_imagenette.pth'
+
+# 10 classes
+CLASS_NAMES = [
+    'tench', 'English springer', 'cassette player', 'chain saw', 'church',
+    'French horn', 'garbage truck', 'gas pump', 'golf ball', 'parachute'
+]
+
+# size of each batch
+BATCH_SIZE = 4
+# testing: 1 x 1, normal: 10 x 10
+DATASET_IMAGE_NUM = 1
+# training dataset path
+DATASET_PATH = '../data/imagenette2-160/val'
+# adv save path
+ADV_SAVE_PATH = 'advs/hop_skip_jump_attack_adv.npy'
+
+
+def init_models():
+  # instantiate resnet model
+  model = utils.load_trained_model(model_name='resnet',
+                                   model_path=MODEL_RESNET_PATH,
+                                   class_num=len(CLASS_NAMES))
+  print('Instantiated ResNet18 with ImageNette trained weights.')
+
+  # # instantiate vgg model
+  # model = utils.load_trained_model(model_name='vgg',
+  #                                      model_path=MODEL_VGG_PATH,
+  #                                      class_num=len(CLASS_NAMES))
+  # print('Instantiated VGG11, with ImageNette trained weights.')
+  return model
+
+
+def attack_switcher(att, fmodel):
+  switcher = {
+      'HopSkipJumpAttack':
+      foolbox.attacks.HopSkipJumpAttack(
+          model=fmodel,
+          distance=foolbox.distances.Linf,
+          criterion=foolbox.criteria.Misclassification()),
+      'SinglePixelAttack':
+      foolbox.attacks.SinglePixelAttack(
+          model=fmodel,
+          distance=foolbox.distances.Linf,
+          criterion=foolbox.criteria.Misclassification())
+  }
+  return switcher.get(att)
+
+
+def main():
+  # load models
+  model = init_models()
+
+  # define preprocessing procedures (Foolbox)
+  preprocessing = dict(mean=[0.485, 0.456, 0.406],
+                       std=[0.229, 0.224, 0.225],
+                       axis=-3)
+
+  # load dataset
+  dataset_loader, dataset_size = utils.load_dataset(
+      dataset_path=DATASET_PATH, dataset_image_len=DATASET_IMAGE_NUM)
+
+  # perform attack for loaded model
+
+  # use GPU if available
+  if torch.cuda.is_available():
+    model = model.cuda()
+
+  # define foolbox wrapper
+  fmodel = foolbox.models.PyTorchModel(model,
+                                       bounds=(0, 1),
+                                       num_classes=len(CLASS_NAMES),
+                                       preprocessing=preprocessing)
+
+  #* 1/3: Validate model's base prediction accuracy (about 97%)
+  print('[TASK 1/3] Validate orginal prediction:')
+  utils.validate(fmodel, dataset_loader, dataset_size, batch_size=BATCH_SIZE)
+
+  #* 2/3: Perform an adversarial attack with blackbox attack
+  print('[TASK 2/3] Generate adversaries:')
+  #TODO: attack methods accepts: HopSkipJumpAttack, SinglePixelAttack
+  attack = attack_switcher('HopSkipJumpAttack')
+
+  tic = time.time()
+  pbar = tqdm(dataset_loader)
+  pbar.set_description('Generate adversaries')
+
+  # iterate through images to generate adversaries
+  adversaries = []
+  for image, label in pbar:
+    #TODO: may need to change according to attack method
+    adv = attack(image.numpy(), label.numpy())
+
+    # if an attack fails under preferred criterions, `np.nan` is returned,
+    #  in which case, we'll return the original image
+    for i, (single_adv, single_img) in enumerate(zip(adv, image.numpy())):
+      if np.isnan(single_adv).any():
+        adv[i] = single_img
+    adversaries.append(adv)
+
+  toc = time.time()
+  time_elapsed = toc - tic
+  pbar.write('Adversaries generated in: {:.2f}m {:.2f}s'.format(
+      time_elapsed // 60, time_elapsed % 60))
+
+  # save generated adversaries
+  np.save(ADV_SAVE_PATH, adversaries)
+
+  #* 3/3: Validate model's adversary predictions
+  print('[TASK 3/3] Validate adversaries:')
+  utils.validate(fmodel,
+                 dataset_loader,
+                 dataset_size,
+                 batch_size=BATCH_SIZE,
+                 advs=adversaries)
+
+  # notify
+  utils.notify(time_elapsed)
+
+
+if __name__ == "__main__":
+  main()
---
+++ blackbox_attack_foolbox/utils/utils.py
@@ -0,0 +1,121 @@
+import os
+import time
+
+import foolbox
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
+
+def load_trained_model(model_name=None, model_path='', class_num=10):
+  # supported models: 'resnet', 'vgg', 'inception', 'mobilenet'
+  model = None
+
+  # load models
+  if model_name == 'resnet':
+    model = torchvision.models.resnet18(pretrained=True)
+    for param in model.parameters():
+      param.requires_grad = False
+    num_features = model.fc.in_features
+    model.fc = nn.Linear(num_features, class_num)
+
+  elif model_name == 'vgg':
+    model = torchvision.models.vgg11(pretrained=True)
+    num_features = model.classifier[-1].in_features
+    model.classifier[-1] = nn.Linear(num_features, class_num)
+
+  elif model_name == 'inception':
+    # TODO: implement inception v3 model loading
+    pass
+
+  elif model_name == 'mobilenet':
+    # TODO: implement mobilenet v2 model loading
+    pass
+
+  else:
+    raise NotImplementedError('Model not supported')
+
+  model.load_state_dict(torch.load(model_path))
+  model.eval()
+  return model
+
+
+def load_dataset(dataset_path=None, dataset_image_len=1, batch_size=4):
+  # resize image to size 213 * 213
+  transform = transforms.Compose(
+      [transforms.Resize((213, 213)),
+       transforms.ToTensor()])
+
+  class_start_indice = [indice * 200 for indice in range(0, dataset_image_len)]
+  images_in_class_indice = np.array(
+      [[j for j in range(k, k + dataset_image_len)]
+       for k in class_start_indice]).flatten()
+
+  # load dataset with validation images
+  dataset = torchvision.datasets.ImageFolder(root=dataset_path,
+                                             transform=transform)
+
+  # 1. get 10 images from 10 classes for a total of 100 images, or ...
+  dataset = torch.utils.data.Subset(dataset, images_in_class_indice)
+  # 2. get first 100 images (all tenches)
+  # dataset = torch.utils.data.Subset(dataset, range(0, 100))
+
+  # compose dataset into dataloader
+  # (don't shuffle, no need to shuffle, we're not training.)
+  dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+  # get dataset size (length)
+  dataset_size = len(dataset)
+
+  print('Loaded data from: {} with a total of {} images.'.format(
+      dataset_path, dataset_size))
+
+  return dataset_loader, dataset_size
+
+
+def validate(fmodel, dataset_loader, dataset_size, batch_size=4, advs=None):
+  pbar = tqdm(dataset_loader)
+
+  # if adv is default (None), validate predictions
+  stage = 'predictions' if advs is None else 'adversaries'
+
+  pbar.set_description('Validate {}'.format(stage))
+  pbar.set_postfix(acc='0.0%')
+
+  preds = []
+  acc = 0.0
+  for i, (image, label) in enumerate(pbar):
+    # make a prediction on either original dataset or adversaries
+    if advs is None:
+      prob = fmodel.forward(image.numpy())
+    else:
+      prob = fmodel.forward(advs[i])
+
+    pred = np.argmax(prob, axis=-1)
+    preds.append(pred)
+
+    # calculate current accuracy
+    acc += np.sum(pred == label.numpy())
+    current_acc = acc * 100 / ((i + 1) * batch_size)
+    pbar.set_postfix(acc='{:.2f}%'.format(current_acc))
+
+  acc = acc * 100 / dataset_size
+  pbar.write('Validated {} with accuracy of: {:.2f}%'.format(stage, acc))
+  return preds
+
+
+def notify(time_elapsed, notify='notify.py'):
+  """
+  Send notifications
+  """
+
+  bitjs = '~/.net/BIT.js'
+  title = 'Attack complete'
+  msg = 'Time elapsed {:.2f}m {:.2f}s'.format(time_elapsed // 60,
+                                              time_elapsed % 60)
+  cmd = 'python {} -b "{}" -t "{}" -m "{}"'.format(notify, bitjs, title, msg)
+  stream = os.popen(cmd)
+  output = stream.read()
+  print('\n' + output)
